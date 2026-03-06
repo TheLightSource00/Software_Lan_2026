edbe074484ebd7ca87dc9cb2c8f7b596aaadc97a
Nur <nnur.nnn000@gmail.com>
2026-03-06 21:42:57 +0300

    Update plan.py

--- plan.py
+++ plan.py
@@ -1,3 +1,3 @@
 print("Nur -Yazılım ve Veri Günlüğü")
-print("Hedef: Çift üniversite eğitimi ile akadmik gelişim.")
+print("Hedef: Cift universite egitimi ile akadmik gelisim.")
 print("Bahar sonu Yapay Zeka (AI) kampı hazırlıkları devam ediyor.")
