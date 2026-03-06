9a89cfde3843ba0529faa80517faad773cc7d1cf
Nur <nnur.nnn000@gmail.com>
2026-03-06 20:51:19 +0300

    Create data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,11 @@
+python
+# Veri Yapıları ve Algoritma Temelleri
+# Bu dosya akademik gelişim sürecimi temsil eder.
+
+def veri_listesi_oluştur():
+  # AI projelerinde kullanılacak temel veri tipleri
+  ogrenci_bilgileri = {
+    "durum": "Cift Universite",
+    "hedef": "AI & Big Data Expert",
+    "plan": " Bahar dönemi sonu AI Camp başlangıcı"
+  }
